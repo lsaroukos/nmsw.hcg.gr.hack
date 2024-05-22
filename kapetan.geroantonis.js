c17aeffd42fbcfc1c815ab8c5cafe71c4f5e13aa
LefterisSaroukos <eleutherios.saroukos@jims.net>
2024-05-23 00:48:16 +0300

    fixed times

--- kapetan.geroantonis.js
+++ kapetan.geroantonis.js
@@ -41,40 +41,40 @@ const LS_AUTOCOMPLETE_CREW = [
  * boat trips definition
  */
 const LS_AUTOCOMPLETE_TRIPS = [
-    {   //to kalymnos
-        title: 'to kalymnos',
-        from: 'kos',
-        to: 'kalymnos',
-        arrived_time : {
-            time: '17:15',
-            yesterday: true
-        },
-        estimated_departure_time : { time: '10:15'},
-        departure_time : { time: '10:15'},
-        estimated_arrival_time : { time: '13:15'},
-        total_people : 0
-    },
     {   //to pserimos
         title: 'to pserimos',
-        from: 'kalymnos',
+        from: 'kos',
         to: 'pserimos',
-        arrived_time : { time: '13:15', yesterday: true },
-        estimated_departure_time : { time: '14:15'},
-        departure_time : { time: '14:10'},
-        estimated_arrival_time : { time: '15:15'},
+        arrived_time : { time: '17:00', yesterday: true },
+        estimated_departure_time : { time: '10:15'},
+        departure_time : { time: '10:15'},
+        estimated_arrival_time : { time: '11:15'},
         total_people : 0
     },
+    {
+        //to kalymnos
+        title: 'to kalymnos',
+        from: 'pserimos',
+        to: 'kalymnos',
+        arrived_time : { time: '11:15', yesterday: true },
+        estimated_departure_time : { time: '12:15'},
+        departure_time : { time: '12:15'},
+        estimated_arrival_time : { time: '13:00'},
+        total_people : 0
+    },
+    
     {   //to kos
         title: 'to kos',
-        from: 'pserimos',
+        from: 'kalymnos',
         to: 'kos',
-        arrived_time : { time: '15:05', yesterday: true },
-        estimated_departure_time : { time: '16:10'},
-        departure_time : { time: '16:05'},
-        estimated_arrival_time : { time: '17:15'},
+        arrived_time : { time: '13:00', yesterday: true },
+        estimated_departure_time : { time: '14:30'},
+        departure_time : { time: '14:30'},
+        estimated_arrival_time : { time: '17:00'},
         total_people : 0
     }
 ];
+
 
 document.addEventListener( 'DOMContentLoaded',()=>{
     /**
